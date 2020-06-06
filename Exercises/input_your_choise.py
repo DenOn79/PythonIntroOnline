e61c2e213f96f342ad35f8981881949fa5749599
DenOn79 <denonufriiev@gmail.com>
2020-06-06 22:41:34 +0300

    three files refactoring

---
+++ Exercises/input_your_choise.py
@@ -0,0 +1,13 @@
+
+number = int(input('Input number from 1 to 10: '))
+if number == 5:
+    print('Your number is equal five!')
+else:
+    if number < 5:
+        print('Your number is less than five')
+    else:
+        if number > 5:
+            print('Your number is more than five')
+
+
+
