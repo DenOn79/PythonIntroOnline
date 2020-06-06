10db890624ba3ae6dd8ed917d1321ebd87494ca9
DenOn79 <denonufriiev@gmail.com>
2020-06-06 19:17:34 +0300

    square_python_file. With thanks

--- Lesson_02/square.py
+++ Lesson_02/square.py
@@ -2,4 +2,4 @@
 
 number = input('This program can help you to find the square of the number.\nPlease, enter your number: ')
 square = (int(number)**2)
-print('The square of', number, 'equals', square)
+print('The square of', number, 'equals', square, '\nThank you for using our product!')
