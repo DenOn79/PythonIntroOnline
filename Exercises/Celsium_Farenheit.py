3e9bbdeaf9bd902a9672a0e6a0d5e708ae11c5f3
DenOn79 <denonufriiev@gmail.com>
2020-06-06 23:21:56 +0300

    Celsium_Fahrenheit modified read_char created

--- Exercises/Celsium_Farenheit.py
+++ Exercises/Celsium_Farenheit.py
@@ -1,5 +1,20 @@
 
-C_temp = input("This program helps to convert temperature value from Celsius scale to Fahrenheit scale.\n"
-               "Please, input Celsius temperature value: ")
-F_temp = 9/5 * float(C_temp) + 32
-print(C_temp,'grades in Celsius scale is equal to', F_temp, 'grades in Fahrenheit scale')
+source_scale_pick = input('This program helps to convert temperature value from Celsius scale to Fahrenheit scale. \n'
+                          'Please choose the scale from which you want to convert value\n'
+                          '\'C\' for Celsius or \'F\' for Fshrenheit: ')
+convert_scale_pick = input('Please choose the scale into which you want to convert value\n'
+                          '\'C\' for Celsius or \'F\' for Fshrenheit: ')
+if source_scale_pick == convert_scale_pick:
+    print('You entered the same scales. What for?')
+else:
+    if source_scale_pick == 'C' or source_scale_pick == 'c' and convert_scale_pick == 'F' or convert_scale_pick == 'f':
+        C_temp = input('Please, input Celsius temperature value: ')
+        F_temp = 9/5 * float(C_temp) + 32
+        print(C_temp,'grades in Celsius scale is equal to', F_temp, 'grades in Fahrenheit scale')
+    else:
+        if source_scale_pick == 'F' or source_scale_pick == 'f' and convert_scale_pick == 'C' or convert_scale_pick == 'c':
+            F_temp = input('Please, input Fahrenheit temperature value: ')
+            C_temp = (float(F_temp) - 32) * 5 / 9
+            print(F_temp, 'grades in Fahrenheit scale is equal to', C_temp, 'grades in Celsius scale')
+
+
---
+++ Exercises/read_char.py
@@ -0,0 +1,12 @@
+
+symbol = input('Please input one of these symbols: A, B or C\n')
+if symbol == 'A' or symbol == 'a':
+    print('\"A\" stands for \"America\"')
+else:
+    if symbol == 'B' or symbol == 'b':
+        print('\"B\" stands for \"Britain\"')
+    else:
+        if symbol == 'C' or symbol == 'c':
+            print('\"C\" stands for \"Canada\"')
+
+
