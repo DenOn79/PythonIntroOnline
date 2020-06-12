f6bbba4015fb4e2f9bef3315976fb31bfff0d30a
DenOn79 <denonufriiev@gmail.com>
2020-06-12 22:39:08 +0300

    Homework_03

--- Lesson_03/biggest_power_of_2.py
+++ Lesson_03/biggest_power_of_2.py
@@ -5,8 +5,7 @@ degree = 1
 while degree <= number:
     degree *= 2
     exponent += 1
-    if degree > number:
-        degree /= 2
-        exponent -= 1
-        break
-print('Degree =', int(degree), '\nexponent =', exponent, '\n2 **', exponent, '=', int(degree))
+else:
+    degree /= 2
+    exponent -= 1
+    print('Degree =', int(degree), '\nexponent =', exponent, '\n2 **', exponent, '=', int(degree))
