7ffe0b3b071e8c00a1d709488f43bc337c417ac0
DenOn79 <denonufriiev@gmail.com>
2020-06-16 23:55:54 +0300

    Homework_04

--- Lesson_04/replace_h_H.py
+++ Lesson_04/replace_h_H.py
@@ -1,5 +1,5 @@
 
-string = input('Input the sentence which contains at least 3 letters \'h\' (e.g. \'She holds her head so high!\'): ')
+string = input('Input the sentence which contains at least 3 letters \'h\' (e.g., \'She holds her head so high!\'): ')
 first = string.find('h')
 last = string.rfind('h')
 print(string[:first+1] + string[first+1: last].replace('h', 'H') + string[last:])
