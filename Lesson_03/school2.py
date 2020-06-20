9cbd27b31ed5a0520cf31fe16a1603569523cb40
DenOn79 <denonufriiev@gmail.com>
2020-06-20 10:09:38 +0300

    Homework_School_3_edition

--- Lesson_03/school2.py
+++ Lesson_03/school2.py
@@ -1,9 +1,12 @@
 
 i = 1
-total_number_of_pupils = 0
+total_number_of_desks = 0
 while i <= 3:
     print('For class #', i)
     number_of_pupils = int(input('Input number of pupils in class: '))
-    total_number_of_pupils += number_of_pupils
+    number_of_desks = ((number_of_pupils+1)//2)
+    print('For class #', i, 'must be bought', number_of_desks, 'desks.')
+    total_number_of_desks += number_of_desks
     i += 1
-print(int((total_number_of_pupils+1)//2), 'desks must be bought for school.')
+print()
+print(int(total_number_of_desks), 'desks must be bought for school.')
