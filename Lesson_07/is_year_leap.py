0f44f1c50471553da3d0fe04948247dae500dafe
DenOn79 <denonufriiev@gmail.com>
2020-06-29 11:40:17 +0300

    Homework07_second edition

--- Lesson_07/is_year_leap.py
+++ Lesson_07/is_year_leap.py
@@ -4,11 +4,14 @@
 
 
 def is_year_leap(year):
-    if year % 4:
-        return False
-    else:
+    if year % 4 == 0 and year % 100 or year % 400 == 0:
         return True
+    else:
+        return False
 
 
 year = int(input('Input year: '))
-print(is_year_leap(year))
+if is_year_leap(year):
+    print(year, 'is leap')
+else:
+    print(year, 'is not leap')
--- Lesson_07/season.py
+++ Lesson_07/season.py
@@ -5,13 +5,13 @@
 
 
 def season(month):
-    if 3 <= month <= 5:
+    if month in (3, 4, 5):
         season = 'Spring'
-    elif 6 <= month <= 8:
+    elif month in (6, 7, 8):
         season = 'Summer'
-    elif 9 <= month <= 11:
+    elif month in (9, 10, 11):
         season = 'Autumn'
-    else:
+    elif month in (12, 1, 2):
         season = 'Winter'
     return season
 
