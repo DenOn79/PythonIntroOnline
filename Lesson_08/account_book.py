025d1100e539c36e44e68bdf01af4d38a3b67a29
DenOn79 <denonufriiev@gmail.com>
2020-07-20 17:57:11 +0300

    Homework08_second edition

--- Lesson_08/account_book.py
+++ Lesson_08/account_book.py
@@ -1,7 +1,7 @@
 
 lst_1 = [34587, 'Learning Python, Mark Lutz', 4, 40.95]
 lst_2 = [98762, 'Programming Python, Mark Lutz', 5, 56.80]
-lst_3 = [77226, 'Head First Python, Paul Barry', 3, 32.95]
+lst_3 = [75226, 'Head First Python, Paul Barry', 3, 32.95]
 lst_4 = [88112, 'Einfuhrung in Python3, Bernard Klein', 3, 24.99]
 total_lst = [lst_1, lst_2, lst_3, lst_4]
 print(total_lst)
